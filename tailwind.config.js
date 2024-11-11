4501b24a63955568ecd1b5251fa46d14e98aa750
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-11-11 16:51:04 +0600

    assets and css theme added

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ export default {
     "./src/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: "#054C73",
+        mediumBlue: "#011C42",
+        darkBlue: "#03344F",
+        lightBlack: "#333333",
+        lightGray: "#F2F5FF",
+        darkGray: "#666666"
+      }
+    },
+    fontFamily: {
+      montserrat: "'Montserrat', sans-serif"
+    }
   },
   plugins: [],
 }
