f345248e0eca6f29c4d13d4bdd37b72e82dbe529
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-11-15 13:56:52 +0600

    Hero mobile styles added

--- src/components/SectionButton/SectionButton.jsx
+++ src/components/SectionButton/SectionButton.jsx
@@ -3,10 +3,12 @@ const SectionButton = (props) => {
     
     return (
         // section button
-        <button className="w-[230px] h-[70px] rounded-[50px] 
-        bg-lightBlue flex justify-center items-center font-montserrat 
-        font-bold text-[16px] text-white transition-all duration-1000 
-        hover:bg-darkBlue active:scale-90">
+        // w-[230px] h-[70px] rounded-[50px] mx-0 text-[16px]
+        <button className="w-[120px] h-[40px] rounded-[30px] 
+        bg-lightBlue mx-auto flex justify-center items-center 
+        font-montserrat font-bold text-[10px] text-white 
+        transition-all duration-1000 hover:bg-darkBlue 
+        active:scale-90">
             {content}
         </button>
     )
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -10,23 +10,30 @@ const Hero = () => {
         style={{backgroundImage: `url(${heroImage})`}}
         className="bg-center bg-cover bg-no-repeat"
     >
-        <Container styles={`px-6 py-[150px] flex justify-end`}>
+        {/* py-[150px] justify-end */}
+        <Container styles={`px-6 py-[50px] flex justify-center`}>
             {/* hero content */}
-            <div className="w-[700px] rounded-[10px] 
-            bg-mediumGray py-[40px] pl-[40px]">
+            {/* w-[700px] py-[40px] pl-[40px] */}
+            <div className="w-[300px] rounded-[10px] 
+            bg-mediumGray py-[40px] px-[40px]">
                 {/* hero content title */}
-                <h6 className="mb-[20px] font-montserrat font-semibold 
-                text-[16px] tracking-[3px] text-lightBlack">
+                {/* mb-[20px] text-[16px] text-start */}
+                <h6 className="mb-[10px] font-montserrat font-semibold 
+                text-[12px] tracking-[3px] text-lightBlack text-center">
                     New Arrival
                 </h6>
                 {/* hero content heading */}
-                <h1 className="font-montserrat font-bold text-[52px] 
-                leading-[65px] text-lightBlue">
+                {/* text-[52px] leading-[65px] text-start */}
+                <h1 className="font-montserrat font-bold text-[30px] 
+                leading-[45px] text-lightBlue text-center">
                     Discover Our New Collection
                 </h1>
                 {/* hero content description */}
-                <p className="mt-[20px] mb-[30px] font-montserrat 
-                font-medium text-[18px] leading-[40px] text-lightBlack">
+                {/* mt-[20px] mb-[30px] text-[18px] leading-[40px] 
+                    text-start */}
+                <p className="mt-[10px] mb-[20px] font-montserrat 
+                font-medium text-[14px] leading-[25px] text-lightBlack 
+                text-center">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.
                 </p>
                 {/* hero content button */}
