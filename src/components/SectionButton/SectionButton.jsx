599508922772a17fdffb844eecfadc31bff675a3
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-11-16 14:07:47 +0600

    Hero tablet styles added

--- src/components/SectionButton/SectionButton.jsx
+++ src/components/SectionButton/SectionButton.jsx
@@ -8,7 +8,8 @@ const SectionButton = (props) => {
         bg-lightBlue mx-auto flex justify-center items-center 
         font-montserrat font-bold text-[10px] text-white 
         transition-all duration-1000 hover:bg-darkBlue 
-        active:scale-90">
+        active:scale-90 xl:w-[230px] xl:h-[70px] xl:rounded-[50px] 
+        xl:mx-0 xl:text-[16px]">
             {content}
         </button>
     )
--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -10,30 +10,30 @@ const Hero = () => {
         style={{backgroundImage: `url(${heroImage})`}}
         className="bg-center bg-cover bg-no-repeat"
     >
-        {/* py-[150px] justify-end */}
-        <Container styles={`px-6 py-[50px] flex justify-center`}>
+        <Container styles={`px-6 py-[50px] flex justify-center 
+            md:justify-end md:py-[100px] xl:py-[150px]`}>
             {/* hero content */}
-            {/* w-[700px] py-[40px] pl-[40px] */}
             <div className="w-[300px] rounded-[10px] 
-            bg-mediumGray py-[40px] px-[40px]">
+            bg-mediumGray py-[40px] px-[40px] md:w-[400px] 
+            md:px-[20px] md:py-[20px] lg:px-[40px] lg:py-[40px] 
+            xl:w-[700px]">
                 {/* hero content title */}
-                {/* mb-[20px] text-[16px] text-start */}
                 <h6 className="mb-[10px] font-montserrat font-semibold 
-                text-[12px] tracking-[3px] text-lightBlack text-center">
+                text-[12px] tracking-[3px] text-lightBlack text-center 
+                xl:mb-[20px] xl:text-[16px] xl:text-start">
                     New Arrival
                 </h6>
                 {/* hero content heading */}
-                {/* text-[52px] leading-[65px] text-start */}
                 <h1 className="font-montserrat font-bold text-[30px] 
-                leading-[45px] text-lightBlue text-center">
+                leading-[45px] text-lightBlue text-center xl:text-[52px] 
+                xl:leading-[65px] xl:text-start">
                     Discover Our New Collection
                 </h1>
                 {/* hero content description */}
-                {/* mt-[20px] mb-[30px] text-[18px] leading-[40px] 
-                    text-start */}
                 <p className="mt-[10px] mb-[20px] font-montserrat 
                 font-medium text-[14px] leading-[25px] text-lightBlack 
-                text-center">
+                text-center xl:mt-[20px] xl:mb-[30px] xl:text-[18px] 
+                xl:leading-[40px] xl:text-start">
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.
                 </p>
                 {/* hero content button */}
