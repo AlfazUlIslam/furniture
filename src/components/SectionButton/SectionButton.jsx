441d821966547e6884661b3c14968cb7dd5c8594
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-11-23 15:35:47 +0600

    Your space mobile and tablet styles added

--- src/components/SectionButton/SectionButton.jsx
+++ src/components/SectionButton/SectionButton.jsx
@@ -1,14 +1,14 @@
 const SectionButton = (props) => {
-    const { content } = props
+    const { content, styles } = props
     
     return (
         // section button
-        <button className="w-[120px] h-[40px] rounded-[30px] 
+        <button className={`${styles} w-[120px] h-[40px] rounded-[30px] 
         bg-lightBlue mx-auto flex justify-center items-center 
         font-montserrat font-bold text-[10px] text-white 
         transition-all duration-1000 hover:bg-darkBlue 
         active:scale-90 xl:w-[230px] xl:h-[70px] xl:rounded-[50px] 
-        xl:mx-0 xl:text-[16px]">
+        xl:mx-0 xl:text-[16px]`}>
             {content}
         </button>
     )
--- src/sections/YourSpace/YourSpace.jsx
+++ src/sections/YourSpace/YourSpace.jsx
@@ -6,24 +6,30 @@ const YourSpace = () => {
   return (
     // Your space
     <section id="your-space">
-        <Container styles={`px-6 py-[60px] flex justify-between 
-        items-center`}>
+        <Container styles={`px-6 py-[60px] flex flex-col 
+        items-center gap-12 md:flex-row md:justify-between 
+        md:gap-0`}>
             {/* Your space content */}
-            <div className="flex flex-col items-start">
+            <div className="flex flex-col items-center 
+            md:items-start">
                 <Heading 
                     content={"Beautify Your Space"}
                 />
                 <SectionDescription 
-                    styles={`w-[490px] mt-[20px] mb-[30px]`}
+                    styles={`w-[280px] mt-[10px] mb-[15px] text-center 
+                    md:text-start lg:w-[490px] lg:mt-[20px] lg:mb-[30px]`}
                     content={"Do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris."}
                 />
                 <SectionButton 
+                    styles={"md:mx-0"}
                     content={"LEARN MORE"}
                 />
             </div>
             {/* Your space image */}
             <div>
                 <img 
+                    className="w-[300px] sm:w-[400px] 
+                    md:w-[300px] lg:w-[400px] xl:w-auto"
                     src={beautifySpaceImage} 
                     alt={"Girl wearing hat"} 
                 />
