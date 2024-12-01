7eb07d2935571656e65c6855b6349f251101d601
AlfazUlIslam <alfaz.ul.islam.mmvii@gmail.com>
2024-12-01 14:52:44 +0600

    Newsletter tablet styles added

--- src/sections/Newsletter/Newsletter.jsx
+++ src/sections/Newsletter/Newsletter.jsx
@@ -10,29 +10,28 @@ const Newsletter = () => {
             <Heading
                 content={"Join Our Mailing List"}
             />
-            {/* w-auto mt-[10px] mb-[50px] */}
             <SectionDescription
-                styles={`w-[280px] mt-[10px] mb-[25px] text-center`}
+                styles={`w-[280px] mt-[10px] mb-[25px] text-center 
+                sm:w-auto md:mb-[50px]`}
                 content={"Sign up to receive inspiration, product updates, and special offers from our team."}
             />
             {/* Newsletter form */}
             <form className="flex justify-center items-center">
-                {/* w-[333px] h-[70px] rounded-tl-[3px] rounded-bl-[3px] 
-                    pl-[25px] text-[16px] */}
                 <input 
                     type="text" 
                     className="w-[200px] h-[40px] rounded-tl-[3px] 
                     rounded-bl-[3px] border border-slate-500 
                     pl-[25px] font-montserrat font-normal text-[12px]
-                    text-lightBlue focus:outline-none focus:border-mediumBlue"
+                    text-lightBlue focus:outline-none focus:border-mediumBlue 
+                    sm:w-[333px] sm:h-[60px] sm:text-[16px] xl:h-[70px]"
                     placeholder="Enter your email"
                 />
-                {/* w-[153px] h-[70px] rounded-[3px] text-[16px] */}
                 <button 
                     className="w-[80px] h-[40px] rounded-[3px] 
                     bg-lightBlue font-montserrat font-bold text-[12px] 
-                    text-white transition-all duration-1000 
-                    hover:bg-darkBlue active:scale-90"
+                    text-white transition-all duration-1000 hover:bg-darkBlue 
+                    active:scale-90 sm:w-[153px] sm:h-[60px] sm:text-[16px] 
+                    xl:h-[70px]"
                     onClick={(e) => e.preventDefault()}
                 >
                     Submit
